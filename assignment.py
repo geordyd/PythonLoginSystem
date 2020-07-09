cc430f4fe0922200acb18f13c894859e3eaa5f2f
Geordy <GeordyCJD@gmail.com>
2020-07-09 16:35:15 +0200

    last

--- assignment.py
+++ assignment.py
@@ -14,9 +14,28 @@ loginState = False
 rootUsername = "1"
 rootPassword = "1"
 
+city = ["Amsterdam", "Rotterdam", "Utrecht", "Eindhoven", "Groningen", "Tilburg", "Almere", "Breda", "Nijmegen", "Apeldoorn"]
 
 def encrypt(data):
     filename = 'userdata.txt'
+    
+    encrypt = ''
+
+    for i in data:
+        position = alphabet.find(i)
+        newposition = (position+key)%len(alphabet)
+        encrypt += alphabet[newposition]
+
+    if os.path.exists(filename):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+    f = open(filename, append_write)
+    f.write(encrypt + "\n")
+    f.close()
+
+def encryptClient(data):
+    filename = 'clientdata.txt'
     
     encrypt = ''
 
@@ -71,25 +90,35 @@ def loginAsRoot(user):
     currentUser = user
     currentRole = "root"
     loginState = True
-    #print("Welcome: " + currentUser + " User Role: " + currentRole + " LoginState: " + str(loginState))
 
 def createUser(role):
     encrypt(username + " " + password + " " + role)
-    
+
+def createClient(firstName, lastName, streetName, houseNumber, zipcode, cityName, email, phoneNumber):
+    encryptClient(firstName + " " + lastName + " " + streetName + " " + houseNumber + " " + zipcode + " " + cityName + " " + email + " " + phoneNumber)
+
+def printCity():
+    print("Choose street index")
+    i = 0
+    while i < len(city):
+        
+        print( str(i) + ": " + city[i])
+        i += 1
+
+def checkStreetIndex(name):
+    i = 0
+    while i < len(city):
+        if name == str(i):
+            return True
+        i += 1
+    return False
+
+
 
 while True:
     
     if loginState == False:
         while True:
-            # print("Login to user")
-            # username  = input("Enter a username:")
-            # password  = input("Enter a password:")
-            # password1 = input("Confirm password:")
-            # if password == password1:
-            #     encrypt(username + " " + password)
-            #     loginState = True
-            #     break
-            # print("Passwords do NOT match!")
             login1 = input("Login:")
             login2 = input("Password:")
             #Login as hardcoded root user
@@ -119,13 +148,15 @@ while True:
                     if rootInput == "c":
                         username  = input("Enter a username:")
                         #Julia validation
-                        validation = userval(username)
-                        if validation == True:
-                            password  = input("Enter a password:")
-                            password1 = input("Confirm password:")
-                        else: 
-                            print("Error")
-                            username
+                        # validation = userval(username)
+                        # if validation == True:
+                        #     password  = input("Enter a password:")
+                        #     password1 = input("Confirm password:")
+                        # else: 
+                        #     print("Error")
+                        #     username
+                        password  = input("Enter a password:")
+                        password1 = input("Confirm password:")
                         if password == password1:
                             createUser("System-Administrator")
                             break
@@ -148,12 +179,26 @@ while True:
                             break
                         print("Passwords do NOT match!")
                     elif systemAdminInput == "n":
-                        username  = input("Enter a username:")
-                        password  = input("Enter a password:")
-                        password1 = input("Confirm password:")
-                        if password == password1:
-                            createUser("client")
-                            break                        
+                        firstName  = input("Enter a First Name:")
+                        lastName = input("Enter a Last Name:")
+                        printCity()
+                        while True:
+                            streetName = input("Enter a Street:")
+                            checkIndex = checkStreetIndex(streetName)
+                            try:
+                                if checkIndex == True:
+                                    break
+                                else:
+                                    raise  ValueError
+                            except ValueError:
+                                print("Error")
+                        houseNumber  = input("Enter a House Number:")
+                        zipcode = input("Enter a Zip Code:")
+                        cityName = input("Enter a City:")
+                        email  = input("Enter an Email:")
+                        phoneNumber = input("Enter a Phone Number:")
+                        createClient(firstName, lastName, streetName, houseNumber, zipcode, cityName, email, phoneNumber)
+                        break                        
 
                     elif systemAdminInput == "l":
                         logout()
@@ -169,43 +214,6 @@ while True:
                         break
                     else:
                         AdvisorInput
-                #     if systemAdminInput == "c":
-                #         username  = input("Enter a username:")
-                #         password  = input("Enter a password:")
-                #         password1 = input("Confirm password:")
-                #         if password == password1:
-                #             createUser("Advisor")
-                #             break
-                #         print("Passwords do NOT match!")
-                #     elif systemAdminInput == "l":
-                #         logout()
-                #         break
-                #     else: 
-                #         AdvisorInput
             
             else:
                 break
-            # login1 = input("Login:")
-            # login2 = input("Password:")
-            # if (login1 == "RootUser1" and login2 == "RootPass1@"):
-            #     currentUser = login1
-            #     currentRole = "Root"
-            #     loginState = True
-            # result = decrypt_all(login1, login2)
-            # if result == True:
-            #     print("Welcome")
-            #     currentUser = login1
-            #     loginState = True
-            #     print("Login State: " + str(loginState))
-            #     print("Username: " + currentUser)
-            # else:
-            #     print("No User")
-            #     break
-            # print("User is logged in.")
-            
-                
-                
-            
-    # if loginState == "Logged In":
-    #         print("Yo " + currentUser)
-    #         yo = input("what now: ")
